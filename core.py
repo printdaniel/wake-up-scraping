e5cb44547f3b46982fd63282853a51d94d95749d
Daniel <dwsuarez98@gmail.com>
2024-01-10 19:28:02 -0300

    menu: opciones y colorama

--- core.py
+++ core.py
@@ -1,17 +1,25 @@
 from diccionario import opciones
 from indices.indice import Indices_economicos
+from colorama import init, Fore
 
 
 
 app = Indices_economicos()
+init()
+
+def opciones_menu():
+    print(Fore.RED + "Indices Económicos: Dolares, Bonos, Riesgo")
 
 
 def ejecutar_menu():
+    opciones_menu()
     while True:
 
-        entrada = input("Elige una opción: ").lower()
+        entrada = input(Fore.YELLOW + "Elige una opción: ").lower()
         
         if entrada in list(opciones.keys()):
             opciones[entrada]()
+        elif entrada == "exit":
+            break
         else:
             print("Opción no válida")
